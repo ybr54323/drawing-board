329719d297025fb50c1caef50d4adc5f1eeaba40
杨秉瑞 <yangbingrui02@countrygarden.com.cn>
2021-11-24 10:38:05 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import rollupPluginCopy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: './',
-  // logLevel:'silent',
-  plugins: [reactRefresh()],
-  compress: {
-    warnings: false,
-    drop_debugger: true,
-    drop_console: true
-  },
+    base: '/',
+    // logLevel:'silent',
+    plugins: [reactRefresh()],
+    compress: {
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true
+    },
 })
